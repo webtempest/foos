c4dab2aa4ed2a735e320301e12af478b69467973
Matthew Platts <plattsmw@gmail.com>
2015-06-22 16:14:13 +1000

    No need for this any more

--- server/seeds.js
+++ server/seeds.js
@@ -35,7 +35,6 @@ Meteor.startup(function () {
 
     var game = {
       completed: false,
-      createdAt: new Date(),
       ownerId: ownerId,
       teams: [
         {name: team1.name, _id: team1._id, score: 0},
